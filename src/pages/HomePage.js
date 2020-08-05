3b0f6a81637d6e36ae56352ac68e94e4d80576b7
adman94 <adman00794@gmail.com>
2020-08-04 21:48:22 -0700

    homepage container added

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import Hero from '../components/Hero'
 import Carousel from "../components/Carousel";
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Container from 'react-bootstrap/Container';
 function HomePage(props) {
     return (
         <div>
-            <Hero 
-            title={props.title}
-            subTitle={props.subTitle}
-            text={props.text}
-            />
-            <Carousel />
+        <Jumbotron className="mt-5 bg-transparent jumbotron-fluid">
+            <Container fluid={true}>
+                <Hero 
+                title={props.title}
+                subTitle={props.subTitle}
+                text={props.text}
+                />
+                <Carousel />
+            </Container>
+        </Jumbotron>
         </div>
         
     )
