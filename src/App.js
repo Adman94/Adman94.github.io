dd24ed7e4b264a3bc179d579c30438c4976c0170
adman94 <adman00794@gmail.com>
2020-08-17 14:10:56 -0700

    fixed link for resume

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ class App extends Component {
             <Nav className="ml-auto">
               <NavLink style={navStyle} className="nav-Link p-2" to="/">Home</NavLink>
               <NavLink style={navStyle} className="nav-Link p-2" to="/about">About</NavLink>
-              <a style={navStyle} className="nav-Link p-2" href="//docs.google.com/document/d/1bimSPzWsdAZcSPFTDRNBxHij0n3ToN3XLXnH7LebBv4/edit?usp=sharing">Resume</a>
+              <a style={navStyle} className="nav-Link p-2" href="https://drive.google.com/file/d/1uSQG5FafTizAyE_5mXZ9XNfhNpKFdPYf/view?usp=sharing">Resume</a>
               <NavLink style={navStyle} className="nav-Link p-2" to="/contact">Contact</NavLink>
             </Nav>
           </Navbar.Collapse>
