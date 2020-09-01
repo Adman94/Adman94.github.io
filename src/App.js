b239dc5f2b00a809fd6ce3940db11ba660b2f0e8
adman94 <adman00794@gmail.com>
2020-09-01 02:38:44 -0700

    updated resume

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Container from 'react-bootstrap/Container';
-import Navbar  from 'react-bootstrap/Navbar';
-import Nav  from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
@@ -11,7 +11,7 @@ import ContactPage from "./pages/ContactPage";
 import './App.css';
 
 const navStyle = {
-  color: 'inherit', 
+  color: 'inherit',
   textDecoration: 'inherit',
 }
 
@@ -21,9 +21,9 @@ class App extends Component {
     this.state = {
       title: "Adeeb Rahman",
       headerLinks: [
-        {title: 'Home', path: '/'},
-        {subTitle: 'About', path: '/about'},
-        {text: 'Contact', path: '/contact'}
+        { title: 'Home', path: '/' },
+        { subTitle: 'About', path: '/about' },
+        { text: 'Contact', path: '/contact' }
       ],
       home: {
         title: 'Welcome Everyone!',
@@ -44,45 +44,45 @@ class App extends Component {
   }
   render() {
     return (
-    <Router>
-      <Container className="p-0" fluid={true}>
-        <Navbar fixed="top" className="overflow-auto border-bottom" bg="white" expand="lg">
-          <NavLink style={navStyle} className="nav-Link p-2" to="/">Adeeb Rahman</NavLink>
-          <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
-          <Navbar.Collapse id="navbar-toggle">
-            <Nav className="ml-auto">
-              <NavLink style={navStyle} className="nav-Link p-2" to="/">Home</NavLink>
-              <NavLink style={navStyle} className="nav-Link p-2" to="/about">About</NavLink>
-              <a style={navStyle} className="nav-Link p-2" href="https://drive.google.com/file/d/1uSQG5FafTizAyE_5mXZ9XNfhNpKFdPYf/view?usp=sharing">Resume</a>
-              <NavLink style={navStyle} className="nav-Link p-2" to="/contact">Contact</NavLink>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
+      <Router>
+        <Container className="p-0" fluid={true}>
+          <Navbar fixed="top" className="overflow-auto border-bottom" bg="white" expand="lg">
+            <NavLink style={navStyle} className="nav-Link p-2" to="/">Adeeb Rahman</NavLink>
+            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <NavLink style={navStyle} className="nav-Link p-2" to="/">Home</NavLink>
+                <NavLink style={navStyle} className="nav-Link p-2" to="/about">About</NavLink>
+                <a style={navStyle} className="nav-Link p-2" href="https://drive.google.com/file/d/1lptVpWyN_KLqzkzIChf9T4TvLP6z-nqf/view?usp=sharing">Resume</a>
+                <NavLink style={navStyle} className="nav-Link p-2" to="/contact">Contact</NavLink>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
 
-        <Route path="/" exact render={() => 
-          <HomePage 
-            title={this.state.home.title}
-            subTitle={this.state.home.subTitle}
-            text={this.state.home.text}
-          />} />
+          <Route path="/" exact render={() =>
+            <HomePage
+              title={this.state.home.title}
+              subTitle={this.state.home.subTitle}
+              text={this.state.home.text}
+            />} />
           <Route path="/about" exact render={() =>
-          <AboutPage 
-            title={this.state.about.title}
-            subTitle={this.state.about.subTitle}
-            text={this.state.about.text}
-          />} />
+            <AboutPage
+              title={this.state.about.title}
+              subTitle={this.state.about.subTitle}
+              text={this.state.about.text}
+            />} />
           <Route path="/contact" exact render={() =>
-          <ContactPage 
-            env={this.props.env}
-            title={this.state.contact.title}
-            subTitle={this.state.contact.subTitle}
-            text={this.state.contact.text}
-          />} />
+            <ContactPage
+              env={this.props.env}
+              title={this.state.contact.title}
+              subTitle={this.state.contact.subTitle}
+              text={this.state.contact.text}
+            />} />
 
-        <Footer />
-      </Container>
-    </Router>
-  );
+          <Footer />
+        </Container>
+      </Router>
+    );
   }
 }
 
